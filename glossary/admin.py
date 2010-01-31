6a12b281d71f9057f0f2a9591d97dfcd04657997
John G. Moylan <john@baijiu.localdomain>
2010-01-31 14:08:44 +0000

    Add Synonyms to admin

--- glossary/admin.py
+++ glossary/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from glossary.models import Term
+from glossary.models import Term, Synonym
 
 class TermAdmin(admin.ModelAdmin):
-	prepopulated_fields = { 'slug': ['title']}
+    prepopulated_fields = { 'slug': ['title']}
+
+class SynonymAdmin(admin.ModelAdmin):
+    pass
 
 admin.site.register(Term, TermAdmin)
+admin.site.register(Synonym, SynonymAdmin)
