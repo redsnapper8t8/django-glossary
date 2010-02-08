bc703750bb710116ba3b066d34409f7ff42a1ab1
John G. Moylan <john@baijiu.localdomain>
2010-02-08 17:27:03 +0000

    fix issue with last commit

--- glossary/models.py
+++ glossary/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 class Term(models.Model):
+    PERSON_TYPE = 1
+    STYLE_TYPE = 2
+    PHILOSOPHY_TYPE = 3
     TYPE_CHOICES = (
         (PERSON_TYPE, 'Person'),
         (STYLE_TYPE, 'Style'),
@@ -9,7 +12,7 @@ class Term(models.Model):
     title = models.CharField(max_length=250, unique=True)
     ipa = models.CharField(max_length=250, unique=True, blank=True)
     slug = models.SlugField(unique=True)
-    description = models.TextField(blank=True, blank=True)
+    description = models.TextField()
     type = models.PositiveSmallIntegerField(choices=TYPE_CHOICES, blank=True)
 
     class Meta:
